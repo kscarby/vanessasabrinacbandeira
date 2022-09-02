b013697fd1ec5e2c2cd5559527364f30dd374338
Vanessa Sabrina <66087295+kscarby@users.noreply.github.com>
2022-09-02 11:06:52 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+ // Ação dos botôes
+
+    function contact() {
+        // seleciona o elemento que deseja modificar
+        var div = document.querySelector(".form");
+        
+        //localiza e mostra através do ip
+        window.location = "#form";
+
+        // modifica o display para "block" fazendo com que o formulário apareça          
+        div.style.display = "block";
+
+    } ;
+    
+    function aboutMe() {
+        
+        // seleciona o elemento que deseja modificar
+        window.location = "#text-aboutme";
+
+    } ;
+
+    function education() {
+        
+        // seleciona o elemento que deseja modificar
+        window.location = "#text-education";
+
+    } ;
+    
+    function skills() {
+        
+        // seleciona o elemento que deseja modificar
+        window.location = "#text-skills";
+
+    } ;  
+
+    function portifolio() {
+        
+        // seleciona o elemento que deseja modificar
+        window.location = "#text-portifolio";
+
+    } ;
